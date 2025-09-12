042136de1a043fe2f445d8d119c109e9633889d7
TouchGrass1 <a4aarush.shah@gmail.com>
2025-09-12 21:12:56 +0100

    updates find track boundaries
    this new method to find the track boundaries relies on the fact that the vector image had slightly different colours for the border of the track, this converts them all to orange and all black to a transparent colour
    however there is an error in differentiate_track_boundaries
    it won't check if a pixel is orange, if so, if it has a blue neighbour it should change to blue too, need to update it

--- pygame window.py
+++ pygame window.py
@@ -1,33 +1,51 @@
 import pygame as pg
 from pygame.locals import *
-import pygame
 
 
-white, red, black, orange, green = (255, 255, 255), (255, 0, 0), (0, 0, 0), (255, 165, 0), (0, 255, 0)
+
+white, red, black, orange, green, clear, blue = (255, 255, 255), (255, 0, 0), (0, 0, 0), (255, 165, 0), (0, 180, 75), (0, 0, 0, 0), (0, 120, 215)
 
 
-def find_track_boundaries_with_background(image):
-    #Find Vertical and horizontal side
-    prev_black = 0 #boolean if the previous colour was black or not
-    for i in range(2):
-        order = [image.get_width(), image.get_height()]
-        for y in range (order[0 + i]): #first does vertical side, then changes the order to do horizontal 
-            for x in range (order[1 - i]):
-                if pg.Surface.get_at(image, (x, y)) == black:
-                    if prev_black == 0:
-                        pg.set_at(image, (x, y), green) #change track boundary to green
-                        prev_black = 1
-                    else: #pixel colour is white or orange
-                        prev_black = 0
-                        if pg.Surface.get_at(image, (x, y)) == white:
-                            pg.set_at(image, (x, y), orange) #change all white to orange cuz mclaren and show that its been checked
+def candidate_neighbors(node):
+    return ((node[0]-1, node[1]-1), (node[0]-1, node[1]), (node[0]-1, node[1]+1), (node[0], node[1]-1), 
+            (node[0], node[1]+1), (node[0]+1, node[1]-1), (node[0]+1, node[1]), (node[0]+1, node[1]+1))
 
-#def find_track_boundaries_no_bg(track):
+def colour_track_boundaries(image, height, width):
+        for y in range (height): #first does vertical side, then changes the order to do horizontal 
+            for x in range (width):
+                if pg.Surface.get_at(image, (x, y))[3] == 0:
+                     continue
+                elif pg.Surface.get_at(image, (x, y)) == blue or pg.Surface.get_at(image, (x, y)) == green: # 2 dots of blue to signify start
+                     start = [x , y]
+                     continue
+                elif pg.Surface.get_at(image, (x, y)) == black:
+                        pg.Surface.set_at(image, (x, y), clear)
+                else:
+                    pg.Surface.set_at(image, (x, y), red) #change all rest to orange cuz mclaren and show that its been checked
+        differentiate_track_boundaries(image, height, width)
+
+def differentiate_track_boundaries(image, height, width):
+    outer_boundary = set()
+    inner_boundary = set()
+    for y in range (height): #first does vertical side, then changes the order to do horizontal 
+        for x in range (width):
+            if pg.Surface.get_at(image, (x, y))[3] == orange:
+                for neighbor in candidate_neighbors((x, y)):
+                    if pg.Surface.get_at(image, neighbor) == blue:
+                        pg.Surface.set_at(image, (x, y), blue)
+                        outer_boundary.add((x, y))
+                    elif pg.Surface.get_at(image, neighbor) == green:
+                        pg.Surface.set_at(image, (x, y), green)
+                        inner_boundary.add((x, y))
+
+
 
 
 def import_tracks():
     silverstone = pg.image.load("tracks/silverstone.png").convert_alpha()
-    return silverstone
+    height = silverstone.get_height()
+    width = silverstone.get_width()
+    return silverstone, height, width
 
 class Button:
     def __init__(self, x, y, w, h, label):
@@ -64,7 +82,9 @@ def main():
     background.blit(text, textpos)
 
     #iport_button = Button(100, 200, 200, 50, "button")    
-    silverstone = import_tracks()
+    silverstone, height, width = import_tracks()
+    colour_track_boundaries(silverstone, height, width)
+
 
     # Event loop
     while True:
