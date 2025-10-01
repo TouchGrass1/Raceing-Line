2ccf3f5c5ede66895c56f75a510ecb46014d31bb
TouchGrass1 <a4aarush.shah@gmail.com>
2025-10-01 21:18:47 +0100

    update drop down
    drop down, clicking on different track names should work now
    blitting text is an issue -BUGGG

--- UI demo.py
+++ UI demo.py
@@ -2,8 +2,9 @@ import pygame as pg
 from pygame.locals import *
 from ComponentModule.components import *
 from colours import colour_palette
-
-
+import time
+from set_up_track import LoadTrack, ValidateTrack, ProcessTrack, OrderOfOperations
+import numpy as np
 
 
 def left_panel(screen_shape, left_panel_border_x):
@@ -24,33 +25,40 @@ def right_panel(background, throttle_val, brake_val, right_panel_border_x, scree
     throttle_slider = Slider(x_margin, int(panel_height * 0.2), slider_width, slider_height, 0, 1, throttle_val, "Throttle", False)
 
     #brake
-    brake_text = Text(x_margin, int(panel_height * 4/15), "brake", font_size) 
+    brake_text = Text(x_margin, int(panel_height * 4/15), "Brake", font_size) 
     brake_text.draw(background)
     brake_slider = Slider(x_margin, int(panel_height * 0.3), slider_width, slider_height, 0, 1, brake_val, "Brake", False)
     return throttle_slider, brake_slider
 
-def top_panel(background, time, lap_no, pb, track_name, weather, screen_shape, font_size, track_list):
-    end_x = int(0.8 * screen_shape[0])
-    even_spacing = int(end_x // 6)
-    x_margin = 0 # screen_shape[0]//128
-    y = 60
-    time_text = Text(x_margin + (even_spacing * 5) , y, f"Time: {time}", font_size)
-    time_text.draw(background)
 
-    lap_text = Text(x_margin + (even_spacing * 4), y, f"Lap: {lap_no}", font_size) 
-    lap_text.draw(background)
+class TopPanel:
+    def __init__(self, background, screen_shape, font_size):
+        self.end_x = int(0.8 * screen_shape[0])
+        self.even_spacing = int(self.end_x // 6)
+        self.x_margin = 0 # screen_shape[0]//128
+        self.y = 60
+        self.font_size = font_size
+        self.background = background
 
-    pb_text = Text(x_margin + (even_spacing * 3), y, f"PB: {pb}", font_size)
-    pb_text.draw(background)
+    def time_display(self, time):
+        time_text = Text(self.x_margin + (self.even_spacing * 5), self.y, f"Time: {time:.2f}", self.font_size)
+        time_text.draw(self.background)
 
-    track_text = Text(x_margin + (even_spacing * 2), y, f"Track: {track_name}", font_size) #temp
-    track_text.draw(background)
-    track_dropdown = Dropdown(x_margin + (even_spacing * 2), y + 40, 150, 30, track_name, track_list)
+    def lap_display(self, lap_no):
+        lap_text = Text(self.x_margin + (self.even_spacing * 4), self.y, f"Lap: {lap_no}", self.font_size) 
+        lap_text.draw(self.background)
+    
+    def pb_display(self, pb):
+        pb_text = Text(self.x_margin + (self.even_spacing * 3), self.y, f"PB: {pb}", self.font_size)
+        pb_text.draw(self.background)
 
-    weather_text = Text(x_margin + even_spacing, y, f"Weather: {weather}", font_size) #temp
-    weather_text.draw(background)
-
-    return track_dropdown
+    def weather_display(self, weather):
+        weather_text = Text(self.x_margin + self.even_spacing, self.y, f"Weather: {weather}", self.font_size) #temp
+        weather_text.draw(self.background)
+    
+    def track_name_display(self, track_name, track_list):
+        track_dropdown = Dropdown(self.x_margin + (self.even_spacing * 2), self.y, 150, 30, track_name, track_list)
+        return track_dropdown
 
 
        
@@ -58,6 +66,8 @@ def top_panel(background, time, lap_no, pb, track_name, weather, screen_shape, f
 def main():
     # Initialise screen
     pg.init()
+    start_time = time.time()
+
     screen = pg.display.set_mode((1920, 1080))
     screen_shape = screen.get_size()
     pg.display.set_caption('Racing Lines')
@@ -78,8 +88,17 @@ def main():
     left_panel_border_x = 0.1 * screen_shape[0]
 
     divididers = [divider(0, top_panel_border_y, right_panel_border_x, 3), divider(right_panel_border_x, 0, 3, screen_shape[1]), divider(left_panel_border_x, top_panel_border_y, 3, screen_shape[1]-top_panel_border_y), divider(right_panel_border_x, 750, 383, 3)]   
-    track_list = ["Monaco", "Silverstone", "Spa", "Interlagos", "Suzuka", "Yas Marina", "Import"]#will be updated with customs tracks that are added
-    track_dropdown = top_panel(background, "0:00.000", 1, "0:00.000", "Monaco", "Sunny", screen_shape, font_size, track_list)
+    track_list = ["Monaco", "Silverstone", "Spa", "Interlagos", "Suzuka", "Yas Marina", "Import"] #will be updated with customs tracks that are added
+
+    top_panel = TopPanel(background, screen_shape, font_size)
+
+    track_name = "Silverstone"
+    track_dropdown = top_panel.track_name_display("Silverstone", track_list)
+    order_of_operations = OrderOfOperations(track_name, dark_mode=True)
+    order_of_operations.run()
+    track_image = order_of_operations.get_track_image()
+
+
     throttle_slider, brake_slider =  right_panel(background, 0.5, 0.9, right_panel_border_x, screen_shape, font_size)
     reset_btn = left_panel(screen_shape, left_panel_border_x)
 
@@ -95,13 +114,32 @@ def main():
         screen.blit(background, (0, 0))
         for d in divididers:
             d.draw(screen)
-            
+        
+        #left panel
         reset_btn.handle_event(event)
         reset_btn.draw(screen, font)
+
+        #right panel
         throttle_slider.draw(screen, font)
         brake_slider.draw(screen, font)
+
+        #top panel
+        top_panel.time_display(time.time() - start_time)
+        top_panel.lap_display(1)
+        top_panel.pb_display("0:00.000")
+        top_panel.weather_display("Sunny")
+
         track_dropdown.handle_event(event)
+        if track_name != track_dropdown.get_track():
+            order_of_operations = OrderOfOperations(track_name, dark_mode=True)
+            order_of_operations.run()
+            track_image = order_of_operations.get_track_image()
+        track_name = track_dropdown.get_track()
         track_dropdown.draw(screen, font)
+
+
+
+        screen.blit(track_image, (500,300))
         
         pg.display.flip()
 
--- set_up_track.py
+++ set_up_track.py
@@ -5,6 +5,7 @@ from math import hypot
 import time
 import os
 
+#track specific colours
 white  = np.array([255, 255, 255], dtype=np.uint8)
 red    = np.array([255, 0, 0], dtype=np.uint8)
 black  = np.array([0, 0, 0], dtype=np.uint8)
@@ -13,152 +14,204 @@ green  = np.array([0, 180, 75], dtype=np.uint8)
 blue   = np.array([0, 81, 186], dtype=np.uint8)
 blue2 = (0, 81, 186)
 green2 = (0, 180, 75)
+BG_GREY = (2, 29, 33)
 
 
-def candidate_neighbors(node, size = 3):
-    nodes = []
-    for i in range(size):
-        for j in range(size):
-            nodes.append((node[0]+i-int(size/2), node[1]+j-int(size/2)))
-    return (nodes)
+class ProcessTrack:
+    def candidate_neighbors(node, size = 3):
+        nodes = []
+        for i in range(size):
+            for j in range(size):
+                nodes.append((node[0]+i-int(size/2), node[1]+j-int(size/2)))
+        return (nodes)
 
-def join_gap(node1, node2):
-    pg.draw.line(track_image, blue, node1, node2)
+    def join_gap(node1, node2):
+        pg.draw.line(track_image, blue, node1, node2)
 
-def colour_track_boundaries(image, height, width):
-        img_arr = pg.surfarray.pixels3d(image) #using surfarray to access pixel values, more efficient as written in C
-        
-        #create masks
-        mask_white  = np.all(img_arr == white, axis=2)
-        mask_black  = np.all(img_arr == black, axis=2)
-        mask_blue   = np.all(img_arr == blue, axis=2)
-        mask_green  = np.all(img_arr == green, axis=2)
-        
-        #apply masks
-        img_arr[mask_black] = white
-        img_arr[~(mask_white | mask_black | mask_blue | mask_green)] = orange
+    def colour_track_boundaries(image, height, width):
+            img_arr = pg.surfarray.pixels3d(image) #using surfarray to access pixel values, more efficient as written in C
+            
+            #create masks
+            mask_white  = np.all(img_arr == white, axis=2)
+            mask_black  = np.all(img_arr == black, axis=2)
+            mask_blue   = np.all(img_arr == blue, axis=2)
+            mask_green  = np.all(img_arr == green, axis=2)
+            
+            #apply masks
+            img_arr[mask_black] = white
+            img_arr[~(mask_white | mask_black | mask_blue | mask_green)] = orange
 
-        for y in range (height): #first does vertical side, then changes the order to do horizontal 
-            for x in range (width):
-                if pg.Surface.get_at(image, (x, y)) == blue2: # 2 dots of blue to signify start
-                    start_blue = [x , y]
-                    continue
-                elif pg.Surface.get_at(image, (x, y)) == green2:
-                    start_green = [x , y]
-                    continue
+            for y in range (height): #first does vertical side, then changes the order to do horizontal 
+                for x in range (width):
+                    if pg.Surface.get_at(image, (x, y)) == blue2: # 2 dots of blue to signify start
+                        start_blue = [x , y]
+                        continue
+                    elif pg.Surface.get_at(image, (x, y)) == green2:
+                        start_green = [x , y]
+                        continue
 
-        order = flood_fill(img_arr, start_blue, start_green)
+            order = ProcessTrack.flood_fill(img_arr, start_blue, start_green)
+            return order
+
+
+    #NOTE: all colouring is done just as a visual aid for debugging, will be removed to imporove performance
+
+    def flood_fill(img_arr, start_blue, start_green):
+
+
+        sets = [set(), set()]
+        sets[0].add((start_blue[0], start_blue[1]))
+        sets[1].add((start_green[0], start_green[1]))
+        colours = [blue, green]
+
+        for i, my_set in enumerate(sets):
+            while my_set:
+                node = my_set.pop()
+                img_arr[node] = colours[i]
+
+                for nx, ny in ProcessTrack.candidate_neighbors(node, size=5):
+                    if 0 <= nx < img_arr.shape[0] and 0 <= ny < img_arr.shape[1]:
+                        if np.array_equal(img_arr[nx, ny], orange):
+                            my_set.add((int(nx), int(ny))) 
+
+        # Ensure tuple conversion when passing
+        order = ProcessTrack.find_order(img_arr, start_blue, start_green, colours)
         return order
 
+            
+    def find_order(img_arr, start_blue, start_green, colours):
+        
+        order = [[start_blue], [start_green]]
 
-#NOTE: all colouring is done just as a visual aid for debugging, will be removed to imporove performance
+        for i in range(2):
+            run = True
+            while run:
+                node = order[i][-1]
+                closest = None
+                closest_dist = np.inf
 
-def flood_fill(img_arr, start_blue, start_green):
+                for size in [3, 5, 11]: # Loop through progressively larger neighborhood sizes
+                    neighbors = ProcessTrack.candidate_neighbors(node, size=size)
+                    for neighbor in neighbors:
+                        if 0 <= neighbor[0] < img_arr.shape[0] and 0 <= neighbor[1] < img_arr.shape[1]:
+                            if np.array_equal(img_arr[neighbor[0], neighbor[1]], colours[i]):
+                                dist = hypot(node[0] - neighbor[0], node[1] - neighbor[1])
+                                if 0 < dist < closest_dist:
+                                    closest = (neighbor[0], neighbor[1])
+                                    closest_dist = dist
+                                    if closest_dist == 1:
+                                        break
+                    if closest is not None:
+                        if size == 11:
+                            ProcessTrack.join_gap(order[i][-1], closest)
+                        break  # stop checking larger sizes
 
+                # If no match was found in any size, stop the run
+                if closest is None:
+                    run = False
+                else:
+                    order[i].append(closest)
+                    temp = len(order[i]) // 24
+                    img_arr[closest] = [temp, 255 - temp, 180]  # update colour
 
-    sets = [set(), set()]
-    sets[0].add((start_blue[0], start_blue[1]))
-    sets[1].add((start_green[0], start_green[1]))
-    colours = [blue, green]
+        
+        ValidateTrack.check_return_to_start(order, start_blue, start_green)
+        return order
 
-    for i, my_set in enumerate(sets):
-        while my_set:
-            node = my_set.pop()
-            img_arr[node] = colours[i]
+    def change_bg_colour(image, dark_mode):
+        img_arr = pg.surfarray.pixels3d(image)
+        print(dark_mode, 'yipppy')
+        if dark_mode:
+            mask = np.all(img_arr == white, axis=2)
+            img_arr[mask] = BG_GREY
+        else:
+            mask = np.all(img_arr == BG_GREY, axis=2)
+            img_arr[mask] = white
 
-            for nx, ny in candidate_neighbors(node, size=5):
-                if 0 <= nx < img_arr.shape[0] and 0 <= ny < img_arr.shape[1]:
-                    if np.array_equal(img_arr[nx, ny], orange):
-                        my_set.add((int(nx), int(ny))) 
+class ValidateTrack:
+    def __init__(self):
+        pass
+    #validation
+    def valid_track(image):
+        img_arr = pg.surfarray.pixels3d(image)
+        
 
-    # Ensure tuple conversion when passing
-    order = find_order(img_arr, start_blue, start_green, colours)
-    return order
+        match_blue = np.all(img_arr == blue, axis = -1)
+        match_green = np.all(img_arr == blue, axis = -1)
+        blue_count = np.sum(match_blue)
+        green_count = np.sum(match_green)
 
-          
-def find_order(img_arr, start_blue, start_green, colours):
-    
-    order = [[start_blue], [start_green]]
-
-    for i in range(2):
-        run = True
-        while run:
-            node = order[i][-1]
-            closest = None
-            closest_dist = np.inf
-
-            for size in [3, 5, 11]: # Loop through progressively larger neighborhood sizes
-                neighbors = candidate_neighbors(node, size=size)
-                for neighbor in neighbors:
-                    if 0 <= neighbor[0] < img_arr.shape[0] and 0 <= neighbor[1] < img_arr.shape[1]:
-                        if np.array_equal(img_arr[neighbor[0], neighbor[1]], colours[i]):
-                            dist = hypot(node[0] - neighbor[0], node[1] - neighbor[1])
-                            if 0 < dist < closest_dist:
-                                closest = (neighbor[0], neighbor[1])
-                                closest_dist = dist
-                                if closest_dist == 1:
-                                    break
-                if closest is not None:
-                    if size == 11:
-                        join_gap(order[i][-1], closest)
-                    break  # stop checking larger sizes
-
-            # If no match was found in any size, stop the run
-            if closest is None:
-                run = False
-            else:
-                order[i].append(closest)
-                temp = len(order[i]) // 24
-                img_arr[closest] = [temp, 255 - temp, 180]  # update colour
-
-    
-    check_return_to_start(order, start_blue, start_green)
-    return order
-
-#validation
-def valid_track(image):
-    img_arr = pg.surfarray.pixels3d(image)
-    
-
-    match_blue = np.all(img_arr == blue, axis = -1)
-    match_green = np.all(img_arr == blue, axis = -1)
-    blue_count = np.sum(match_blue)
-    green_count = np.sum(match_green)
-
-    if blue_count == 1 and green_count == 1:
-        return True
-
-    else: 
-        print('you have',blue_count,'blues and',green_count,'greens \n blue value: ',blue, 'green value: ',green)
-        return False
-
-def check_return_to_start(order, start_blue, start_green):
-    start = [start_blue, start_green]
-    for i in range(2):
-        if hypot(order[i][-1][0] - start[i][0], order[i][-1][1] - start[i][1]) < 1.5:
-            print("Returned to start")
+        if blue_count == 1 and green_count == 1:
             return True
 
-#load and save   
-def save_order(order, track_name):
-    filename = f"{track_name}_order.npy"
-    np.save(os.path.join('orders', filename), order)
-    print(f"Order saved as {filename}")
+        else: 
+            print('you have',blue_count,'blues and',green_count,'greens \n blue value: ',blue, 'green value: ',green)
+            return False
 
-def load_order(track_name):
-    filename = f"{track_name}_order.npy"
-    try:
-        return np.load(os.path.join('orders', filename), allow_pickle=True)
-    except FileNotFoundError:
-        print("No saved order found")
-        return None
-    
-def import_tracks(track_name= 'silverstone'):
-    global track_image
-    track_image  = pg.image.load(f"Assets/tracks/{track_name}.png").convert_alpha()
-    height = track_image.get_height()
-    width = track_image.get_width()
-    return track_image, height, width
+    def check_return_to_start(order, start_blue, start_green):
+        start = [start_blue, start_green]
+        for i in range(2):
+            if hypot(order[i][-1][0] - start[i][0], order[i][-1][1] - start[i][1]) < 1.5:
+                print("Returned to start")
+                return True
+
+class LoadTrack:
+    def __init__(self):
+        pass
+
+    def save_order(order, track_name):
+        filename = f"{track_name}_order.npy"
+        np.save(os.path.join('orders', filename), order)
+        print(f"Order saved as {filename}")
+
+    def load_order(track_name):
+        filename = f"{track_name}_order.npy"
+        try:
+            return np.load(os.path.join('orders', filename), allow_pickle=True)
+        except FileNotFoundError:
+            print("No saved order found")
+            return None
+        
+    def import_tracks(track_name= 'silverstone'):
+        global track_image
+        try:
+            track_image  = pg.image.load(f"Assets/tracks/{track_name}.png").convert_alpha()
+        except FileNotFoundError:
+            print("FileNotFoundError: Track image not found.")
+            return None
+        height = track_image.get_height()
+        width = track_image.get_width()
+        return track_image, height, width
+
+class OrderOfOperations:
+    def __init__(self, track_name, dark_mode=False):
+        self.track_name = track_name.lower()
+        self.track_image = None
+        self.height = None
+        self.width = None
+        self.order = None
+        self.dark_mode = dark_mode
+
+    def run(self):
+        self.track_image, self.height, self.width = LoadTrack.import_tracks(self.track_name)
+        if self.track_image is None:
+            print("file not found")
+        else:
+            self.order = LoadTrack.load_order(self.track_name)
+            if self.order is None:
+                if ValidateTrack.valid_track(self.track_image):
+                    self.order = ProcessTrack.colour_track_boundaries(self.track_image, self.height, self.width)
+                    self.order = np.array(self.order, dtype=object)
+                    LoadTrack.save_order(self.order, self.track_name)
+            ProcessTrack.change_bg_colour(self.track_image, self.dark_mode)
+    def get_track_image(self):
+        return self.track_image
+    def get_order(self):
+        return self.order
+    def get_dim(self):
+        return self.width, self.height
+
+
 
 
 
@@ -186,13 +239,11 @@ def main():
 
     #iport_button = Button(100, 200, 200, 50, "button")
     track_name = 'silverstone' 
-    track_image, height, width = import_tracks(track_name)
-    order = load_order(track_name)
-    if order is None:
-        if valid_track(track_image):
-            order = colour_track_boundaries(track_image, height, width)
-            order = np.array(order, dtype=object)
-            save_order(order, track_name)
+    order_of_operations = OrderOfOperations(track_name)
+    order_of_operations.run()
+    track_image = order_of_operations.get_track_image()
+
+
 
     print("Time taken to find order:", time.time() - start)
 
@@ -205,8 +256,8 @@ def main():
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 return
         screen.blit(background, (0, 0))
+        
         screen.blit(track_image, (500,300))
-
         
         pg.display.flip()
 
